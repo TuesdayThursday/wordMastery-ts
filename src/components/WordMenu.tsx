753ffcb3830b9f0ce4437d8229b465ae077743d5
AYAYze <ehdud2004@gmail.com>
2023-02-14 15:33:29 +0900

    Translate and listId context

--- src/components/WordMenu.tsx
+++ src/components/WordMenu.tsx
@@ -1,21 +1,41 @@
-import React, { useContext } from "react";
-import { menuContext } from "../unit/WordSeparator";
-
+import React, { useContext, useState, useEffect } from "react";
+import { listIdContext } from "../routes/WordLearn";
+import { addWord } from "../unit/SentenceManager";
+import { menuContext } from "./WordSeparator";
+import { translator } from "../unit/Translator"
 const dictionaryBaseURL = "https://en.dict.naver.com/#/search?range=word&query=";
 
 function WordMenu(prop:{word:string, className:string}) {
+    const listIdContexts = useContext(listIdContext)
     const MenuContexts = useContext(menuContext)
+    const [translatedWord, setTranslatedWord] = useState(prop.word);
 
     let wordTrimRegExp = new RegExp(/([A-Za-z-])+/gi);
     let wordTrim = '';
-    if(prop.word) {
-        let temp = wordTrimRegExp.exec(prop.word);
-        wordTrim = temp ? temp[0] : '';
-    }
 
-    function addWord(){
+    useEffect(()=>{
+        if(prop.word) {
+            let temp = wordTrimRegExp.exec(prop.word);
+            wordTrim = temp ? temp[0] : '';
+            // translator(wordTrim, 0, 1)
+            // .then(res=>res.json())
+            // .then(data => {
+            //     console.log(data);
+            //     setTranslatedWord(data);
+            // })
+        }
+
+    },[])
+
+    function addWordBtnHandler(){
         MenuContexts?.changeWordId(-1);
-        
+        if(listIdContexts?.wordListId)
+            addWord(listIdContexts?.wordListId, wordTrim)
+            .then(res=>res.json())
+            .then(data=>
+                console.log(data)    
+            )
+
     }
 
     function searchDictionary() {
@@ -28,14 +48,18 @@ function WordMenu(prop:{word:string, className:string}) {
     }
     
 
+
     return (
         <div className={prop.className}>
             <ul>
-                <li onClick={addWord}>
+                <li onClick={addWordBtnHandler}>
                     단어 추가
                 </li>
                 <li onClick={searchDictionary}>
-                    단어 뜻
+                    {
+                    //단어 뜻
+                    translatedWord
+                    }
                 </li>
             </ul>
         </div>
